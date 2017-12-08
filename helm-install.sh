d9560026a3064bdd9382d94b54e660a24cd15992
svicente <sergiovicente@es.ibm.com>
2017-12-08 16:56:04 +0100

    modified.sh

--- helm-install.sh
+++ helm-install.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-helm install --name fnproject --set fn.service.type=NodePort,fn.service.port=32180,ui.service.type=NodePort,redis.usePassword=false fn
-=======
-helm install --name fnproject --set fnserver.service.type=NodePort,ui.service.type=NodePort,redis.usePassword=false fn
->>>>>>> 11e1bafe1c444301873d6da1e865e7bed9667c48
+helm install --name fnproject --set fn.service.type=NodePort,ui.service.type=NodePort,redis.usePassword=false fn
