4feb01122c2399aa847307dc7c20de42d977b841
svicente <sergiovicente@es.ibm.com>
2017-12-11 00:58:51 +0100

    redis image to bitnami/redis:latest

--- helm-install.sh
+++ helm-install.sh
@@ -1,1 +1,1 @@
-helm install --name fnproject --set fn.service.type=NodePort,ui.service.type=NodePort,redis.usePassword=false fn
+helm install --name fnproject --set fn.service.type=NodePort,ui.service.type=NodePort,redis.image=bitnami/redis:latest,redis.usePassword=false fn
